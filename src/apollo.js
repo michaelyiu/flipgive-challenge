3ea00d053b6370a952e67c023179e79e769d321f
unknown <onew1ng3d@gmail.com>
2020-09-23 22:12:09 -0400

    last push

--- src/apollo.js
+++ src/apollo.js
@@ -4,15 +4,14 @@ import { setContext } from '@apollo/client/link/context';
 const httpLink = createHttpLink({
 	uri: 'https://api.github.com/graphql',
 });
-
 const authLink = setContext((_, { headers }) => {
-	// get the authentication token from local storage if it exists
-	// const token = localStorage.getItem('token');
-	// return the headers to the context so httpLink can read them
+	// const token = process.env.REACT_APP_GITHUB;
+
 	return {
 		headers: {
 			...headers,
-			authorization: `Bearer a2effa0ad9ea10c1068bdd53684d40be78ebcb3d`
+			// authorization: token ? token : null
+			authorization: `bearer 6f2efacd542098596874d7b51e509a65fb3e3abe`
 		}
 	}
 });
