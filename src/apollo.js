0213f64b9cb2f95ddf70e9dcb23fa0d87a25a4be
unknown <onew1ng3d@gmail.com>
2020-09-23 22:38:35 -0400

    env var replace hardcoded access token

--- src/apollo.js
+++ src/apollo.js
@@ -1,17 +1,17 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+require('dotenv').config();
+
 const httpLink = createHttpLink({
 	uri: 'https://api.github.com/graphql',
 });
 const authLink = setContext((_, { headers }) => {
-	// const token = process.env.REACT_APP_GITHUB;
-
+	const token = `bearer ${process.env.REACT_APP_GITHUB}`;
 	return {
 		headers: {
 			...headers,
-			// authorization: token ? token : null
-			authorization: `bearer 6f2efacd542098596874d7b51e509a65fb3e3abe`
+			authorization: token ? token : null
 		}
 	}
 });
